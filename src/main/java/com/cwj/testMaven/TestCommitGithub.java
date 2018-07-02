fda0c634acf64e588ee21fa6cede8411e643c4d1
123 <964058107@qq.com>
2018-07-02 13:41:52 +0800

    修改了类TestCommitGithub

--- src/main/java/com/cwj/testMaven/TestCommitGithub.java
+++ src/main/java/com/cwj/testMaven/TestCommitGithub.java
@@ -6,6 +6,6 @@ package com.cwj.testMaven;
 public class TestCommitGithub {
     public static void main(String[] args){
         System.out.println("123");
-        System.out.println("hihihi!~girl");
+        System.out.println("hihihi!~girl11");
     }
 }
