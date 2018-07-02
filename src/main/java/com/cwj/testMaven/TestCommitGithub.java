bd941cd02042587b30f9a050fbff9b062af7378e
123 <964058107@qq.com>
2018-07-02 13:05:03 +0800

    增加了类TestCommitGithub

---
+++ src/main/java/com/cwj/testMaven/TestCommitGithub.java
@@ -0,0 +1,10 @@
+package com.cwj.testMaven;
+
+/**
+ * Created by 96405 on 2018/7/2.
+ */
+public class TestCommitGithub {
+    public static void main(String[] args){
+        System.out.println("123");
+    }
+}
